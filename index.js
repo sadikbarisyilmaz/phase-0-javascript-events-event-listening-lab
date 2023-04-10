9df7ffe232ea877010df25ad21f44caa5d25a9cb
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-10 13:18:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('button');
 function addingEventListener() {
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener()
